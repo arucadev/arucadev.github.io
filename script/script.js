627435926833dc2fd7cb0fdcb085e2f6f2f92e2d
Alex Ruiz <arucadev@gmail.com>
2024-11-19 18:44:43 +0100

    Update script.js
    Test

--- script/script.js
+++ script/script.js
@@ -3,12 +3,11 @@ const fadeIndicator = document.querySelector('.fade-scroll');
 // Showing fade if user is scrolling
 function toggleFadeIndicator() {
   const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
-  if (window.scrollY < scrollableHeight - 25) {
+  if (window.scrollY < scrollableHeight - 20) {
     fadeIndicator.style.opacity = '1';
   } else {
     fadeIndicator.style.opacity = '0';
   }
-  console.log({ scrollY, scrollableHeight })
 }
 
 // Listening scroll, load and resize event
