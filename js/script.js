d0b868353916a3d4f9e42fa53b6ef135e1bdc6d6
Alex Ruiz <arucadev@gmail.com>
2024-12-12 15:35:26 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -51,3 +51,64 @@ document.body.removeChild(tempInput); // Removes the temporal input
 
 alert('Email address copied to clipboard!'); // Alerts user
 });
+
+// TOGGLE THEME
+// Elements
+const toggleCheckbox = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
+// SVGs
+const lightModeSVG = `
+    <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+    <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2"></path>
+    <path d="M19 11h2m-1 -1v2"></path>
+`;
+
+const darkModeSVG = `
+    <path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z"></path>
+    <path d="M6.343 17.657l-1.414 1.414"></path>
+    <path d="M6.343 6.343l-1.414 -1.414"></path>
+    <path d="M17.657 6.343l1.414 -1.414"></path>
+    <path d="M17.657 17.657l1.414 1.414"></path>
+    <path d="M4 12h-2"></path>
+    <path d="M12 4v-2"></path>
+    <path d="M20 12h2"></path>
+    <path d="M12 20v2"></path>
+`;
+
+/**
+ * Apply saved theme
+ */
+function applySavedTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    const isDarkMode = savedTheme === 'dark';
+
+    // Aplicar clase y estado del checkbox
+    document.documentElement.classList.toggle('darkmode', isDarkMode);
+    toggleCheckbox.checked = isDarkMode;
+
+    // Cambiar ícono del SVG
+    themeIcon.innerHTML = isDarkMode ? darkModeSVG : lightModeSVG;
+}
+
+/**
+ * Alternate between light and dark theme
+ */
+function toggleTheme() {
+    const isDarkMode = toggleCheckbox.checked;
+
+    // Alternate root class
+    document.documentElement.classList.toggle('darkmode', isDarkMode);
+
+    // localStorage
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+
+    // Change icon svg
+    themeIcon.innerHTML = isDarkMode ? darkModeSVG : lightModeSVG;
+}
+
+// Events
+toggleCheckbox.addEventListener('change', toggleTheme);
+
+// Init
+applySavedTheme();
